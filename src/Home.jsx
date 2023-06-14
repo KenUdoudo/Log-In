a4f5eeefdf7516da7915ba6bc706363577919ce3
Ken <kendo302003@gmail.com>
2023-06-14 16:16:56 +0100

    margin top

--- src/Home.jsx
+++ src/Home.jsx
@@ -2,7 +2,7 @@ import evon from './assets/logo (1).png'
 const Home = () => {
   return (
     <div className="flex flex-col justify-center items-center">
-      <div className='flex flex-col justify-center items-center'>
+      <div className='flex flex-col justify-center items-center mt-40'>
       <img src={ evon }/>
       <h2>EVON</h2>
       </div>
