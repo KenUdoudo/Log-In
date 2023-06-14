11c1c2848f72079efa59dfb395ee2b00f8495528
Ken <kendo302003@gmail.com>
2023-06-14 16:13:47 +0100

    justify

--- src/Home.jsx
+++ src/Home.jsx
@@ -1,12 +1,12 @@
 import evon from './assets/logo (1).png'
 const Home = () => {
   return (
-    <div className="flex flex-col">
+    <div className="flex flex-col justify-center items-center">
       <div className='flex flex-col'>
       <img src={ evon }/>
       <h2>EVON</h2>
       </div>
-    <p className='text-center'>Discover upcoming events near you</p>
+     <p className='text-center'>Discover upcoming events near you</p>
     </div>
   );
 }
