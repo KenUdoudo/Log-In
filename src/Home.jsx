5be123bfedb6d810eef53277a8f9c9efa91f9249
Ken <kendo302003@gmail.com>
2023-06-14 16:10:59 +0100

    fixing error

--- src/Home.jsx
+++ src/Home.jsx
@@ -2,12 +2,12 @@ import evon from './assets/logo (1).png'
 const Home = () => {
   return (
     <div className="flex flex-col">
-    <div className='flex flex-col'>
+      <div className='flex flex-col'>
       <img src={ evon }/>
       <h2>EVON</h2>
-    </div>
+      </div>
     <p className='text-center'>Discover upcoming events near you</p>
-    <div/>
+    </div>
   );
 }
  
