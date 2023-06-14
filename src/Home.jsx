4c77fde633b454305077ac2b47396e69f25d5a67
Ken <kendo302003@gmail.com>
2023-06-14 16:10:25 +0100

    trying something with flex

--- src/Home.jsx
+++ src/Home.jsx
@@ -1,13 +1,13 @@
 import evon from './assets/logo (1).png'
 const Home = () => {
   return (
-    <>
-    <div>
+    <div className="flex flex-col">
+    <div className='flex flex-col'>
       <img src={ evon }/>
       <h2>EVON</h2>
     </div>
     <p className='text-center'>Discover upcoming events near you</p>
-    </>
+    <div/>
   );
 }
  
