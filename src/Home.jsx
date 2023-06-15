ff5b6b7b47d9980d64d5326a314d2d4bbfd3dfde
Ken <kendo302003@gmail.com>
2023-06-15 17:03:25 +0100

    button styling

--- src/Home.jsx
+++ src/Home.jsx
@@ -9,7 +9,7 @@ const Home = () => {
      <p className='text-center text-2xl mt-10'>Discover upcoming events near you</p>
 
      <div className='mt-20 flex flex-col justify-center items-center'>
-      <button className=''>Sign Up</button>
+      <button className='border rounded py-2'>Sign Up</button>
       <button>Log In</button>
      </div>
      <a href="#"><p>Skip For Now</p></a>
