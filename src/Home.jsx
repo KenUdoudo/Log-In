ee9693d807df43559aab5b8ec6e70c709fd83c27
Ken <kendo302003@gmail.com>
2023-06-17 07:57:51 +0100

    linking sign up

--- src/Home.jsx
+++ src/Home.jsx
@@ -1,4 +1,6 @@
 import evon from './assets/logo (1).png'
+import { Link } from 'react-router-dom'
+
 const Home = () => {
   return (
     <div className="flex flex-col justify-center items-center">
@@ -9,7 +11,7 @@ const Home = () => {
      <p className='text-center text-2xl mt-10'>Discover upcoming events near you</p>
 
      <div className='mt-20 flex flex-col justify-center items-center'>
-      <button className='border-0 rounded py-2 px-24 text-lg bg-blue-600 text-white'>Sign Up</button>
+      <Link to="/SignUp"><button className='border-0 rounded py-2 px-24 text-lg bg-blue-600 text-white'>Sign Up</button></Link>
       <button className='border border-blue-600 text-blue-600 rounded py-2 px-24 text-xl mt-4'>Log In</button>
      </div>
      <a href="#" className='mt-10'><p className=' text-blue-600'>Skip For Now</p></a>
