1faa36791e11f417f5358e6f4d14844a03435027
Ken <kendo302003@gmail.com>
2023-06-17 07:51:24 +0100

    importing sign up

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import { useState } from 'react'
 import './index.css'
 import Home from './Home'
 import { Route, Routes } from 'react-router-dom'
+import SignUp from './Signup'
 
 function App() {
   
