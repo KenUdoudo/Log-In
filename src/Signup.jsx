0ba194de74ecb89cc0c2e1d177c6a64b59973e81
Ken <kendo302003@gmail.com>
2023-06-18 12:37:33 +0100

    form flex

--- src/Signup.jsx
+++ src/Signup.jsx
@@ -3,7 +3,7 @@ const SignUp = () => {
   return (  
     <div className='flex flex-col justify-center items-center'>
       <img src={ evon }/>
-      <form>
+      <form className='flex flex-col justify-center items-center'>
         <input type="text" placeholder='Full name' required/>
         <input type="email" placeholder='Email' required/>
         <input type="password" placeholder='Password' required/>
