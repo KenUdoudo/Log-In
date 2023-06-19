291685cf235a3414a8fdf72b21ed21f437981cd2
Ken <kendo302003@gmail.com>
2023-06-19 06:58:15 +0100

    input css

--- src/Signup.jsx
+++ src/Signup.jsx
@@ -4,7 +4,7 @@ const SignUp = () => {
     <div className='flex flex-col justify-center items-center'>
       <img src={ evon } className='mt-20'/>
       <form className='flex flex-col justify-center items-center mt-20'>
-        <input type="text" placeholder='Full name' className='border-0 rounded bg-gray-200' required/>
+        <input type="text" placeholder='Full name' className='border-0 rounded bg-gray-200 pl-4 py-1 pr-5 text-xl' required/>
         <input type="email" placeholder='Email' required/>
         <input type="password" placeholder='Password' required/>
         <button type="submit">Sign Up</button>
