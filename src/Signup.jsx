78f5a56088e79e328b1f97a1116c2217225284a1
Ken <kendo302003@gmail.com>
2023-06-19 06:56:01 +0100

    trying full width

--- src/Signup.jsx
+++ src/Signup.jsx
@@ -3,7 +3,7 @@ const SignUp = () => {
   return (  
     <div className='flex flex-col justify-center items-center'>
       <img src={ evon } className='mt-20'/>
-      <form className='flex flex-col justify-center items-center mt-20 mx-10'>
+      <form className='flex flex-col justify-center items-center mt-20'>
         <input type="text" placeholder='Full name' className='border-0 rounded bg-gray-200' required/>
         <input type="email" placeholder='Email' required/>
         <input type="password" placeholder='Password' required/>
