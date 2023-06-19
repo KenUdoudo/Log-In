8b3767a9485d64c7fd3ff571bc8ee89028fda802
Ken <kendo302003@gmail.com>
2023-06-19 06:50:28 +0100

    mt

--- src/Signup.jsx
+++ src/Signup.jsx
@@ -2,7 +2,7 @@ import evon from './assets/logo (1).png'
 const SignUp = () => {
   return (  
     <div className='flex flex-col justify-center items-center'>
-      <img src={ evon }/>
+      <img src={ evon } className='mt-20'/>
       <form className='flex flex-col justify-center items-center'>
         <input type="text" placeholder='Full name' required/>
         <input type="email" placeholder='Email' required/>
