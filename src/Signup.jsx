60b5d34201d4b6164336112a08fc9c816db33dd3
Ken <kendo302003@gmail.com>
2023-06-22 09:20:14 +0100

    last edit

--- src/Signup.jsx
+++ src/Signup.jsx
@@ -5,14 +5,14 @@ import { Link } from 'react-router-dom'
 const SignUp = () => {
   return (  
     <div className='flex flex-col justify-center items-center'>
-      <img src={ evon } className='mt-20'/>
+      <img src={ evon } className='mt-14'/>
       <form className='flex flex-col justify-center items-center mt-20 gap-y-4'>
         <input type="text" placeholder='Full name' className='border-0 rounded bg-gray-200 pl-4 py-1 pr-5 text-xl' required/>
         <input type="email" placeholder='Email' className='border-0 rounded bg-gray-200 pl-4 py-1 pr-5 text-xl' required/>
         <input type="password" placeholder='Password' className='border-0 rounded bg-gray-200 pl-4 py-1 pr-5 text-xl' required/>
         <button type="submit" className='border-0 rounded bg-blue-600 px-24 py-1 text-lg text-white'>Sign Up</button>
       </form>
-      <div className='flex flex-row justify-center items-center mt-4'>
+      <div className='flex flex-row justify-center items-center my-2'>
         <hr />
         <p>or</p>
         <hr />
@@ -25,6 +25,7 @@ const SignUp = () => {
           <img src={ google } className='px-3  py-2'/>
           <p className='bg-gray-300 pr-5 pl-2 py-2 text-base font-semibold'>Sign up with Google</p>
         </div>
+        <p className='mt-20'>Already have an account? <Link to='/LogIn'><span className='text-blue-600'>Log In</span></Link></p>
     </div>
   );
 }
