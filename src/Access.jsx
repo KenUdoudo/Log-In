46c1e7d9b44394bd8b542b8eff06e0c56122555b
Ken <kendo302003@gmail.com>
2023-06-23 17:45:31 +0100

    adding img

--- src/Access.jsx
+++ src/Access.jsx
@@ -2,7 +2,9 @@ import evon from './assets/logo (1).png'
 const Access = () => {
   return (  
     <div>
-
+      <div className='flex flex-col justify-center items-center'>
+      <img src={ evon } className='mt-10'/>
+      </div>
     </div>
   );
 }
