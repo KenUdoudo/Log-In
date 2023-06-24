ed424459047080f7eec95080180fdca6bc59fddf
Ken <kendo302003@gmail.com>
2023-06-24 17:25:57 +0100

    margin top

--- src/Access.jsx
+++ src/Access.jsx
@@ -5,9 +5,9 @@ const Access = () => {
       <div className='flex flex-col justify-center items-center'>
       <img src={ evon } className='mt-10'/>
       </div>
-      <p className='text-center text-gray-400 font-semibold text-3xl'>Welcome! to Evon</p>
-      <div>
-        <div></div>
+      <p className='text-center text-gray-400 font-semibold text-3xl mt-10'>Welcome! to Evon</p>
+      <div className='flex flex-col justify-center items-center'>
+        <div className='bg-gray-400 border-0 rounded'></div>
         <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Doloribus, dolore! Cupiditate deleniti ut possimus, quisquam, magnam voluptatibus vel maxime doloremque quis nobis sint! Vel saepe sapiente, aperiam possimus praesentium unde!</p>
       </div>
     </div>
