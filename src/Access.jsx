1e1503b51ab301a71850ec4683ce79b72d474935
Ken <kendo302003@gmail.com>
2023-06-24 17:19:55 +0100

    winging it

--- src/Access.jsx
+++ src/Access.jsx
@@ -5,6 +5,7 @@ const Access = () => {
       <div className='flex flex-col justify-center items-center'>
       <img src={ evon } className='mt-10'/>
       </div>
+      <p>Welcome! to Evon</p>
     </div>
   );
 }
