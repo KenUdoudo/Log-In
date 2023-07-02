f3773fa0497b9efb71f999157bf015bd82e27442
Ken <kendo302003@gmail.com>
2023-07-02 08:09:23 +0100

    removing link to sign up

--- src/Signup.jsx
+++ src/Signup.jsx
@@ -3,7 +3,6 @@ import facebook from './assets/logo (3).png'
 import google from './assets/logo (2).png'
 import { Link } from 'react-router-dom'
 const SignUp = () => {
-  
   return (  
     <div className='flex flex-col justify-center items-center'>
       <img src={ evon } className='mt-14'/>
