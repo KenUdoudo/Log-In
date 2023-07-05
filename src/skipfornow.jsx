f2100910c1a3d86411f95bc25f8d8007392cc391
Ken <kendo302003@gmail.com>
2023-07-05 06:45:45 +0100

    importing other imgs

--- src/skipfornow.jsx
+++ src/skipfornow.jsx
@@ -1,8 +1,12 @@
 import one from './assets/wallpaperflare.com_wallpapper (6).jpg'
+import two from './assets/wallpaperflare.com_wallpapper (7).jpg'
+import three from './assets/wallpaperflare.com_wallpapper (8).jpg'
+import four from './assets/wallpaperflare.com_wallpapper (9).jpg'
+import five from './assets/wallpaperflare.com_wallpapper (7).jpg'
 const Skip = () => {
   return ( 
     <div>
-
+      
     </div>
   );
 }
