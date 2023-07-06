c10cb9df6fe7d6790fe1337019ccce174ea94cc6
Ken <kendo302003@gmail.com>
2023-07-06 05:58:23 +0100

    fixed error

--- src/skipfornow.jsx
+++ src/skipfornow.jsx
@@ -1,12 +1,12 @@
-
-import two from './assets/wallpaperflare.com_wallpapper (7).jpg'
-import three from './assets/wallpaperflare.com_wallpapper (8).jpg'
-import four from './assets/wallpaperflare.com_wallpapper (9).jpg'
-import five from './assets/wallpaperflare.com_wallpapper (7).jpg'
+import One from './assets/wallpaperflare.com_wallpaper (6).jpg'
+import two from './assets/wallpaperflare.com_wallpaper (7).jpg'
+import three from './assets/wallpaperflare.com_wallpaper (8).jpg'
+import four from './assets/wallpaperflare.com_wallpaper (9).jpg'
+import five from './assets/wallpaperflare.com_wallpaper (7).jpg'
 const Skip = () => {
   return ( 
     <div>
-     
+      <img src={ One }/>
       <img src={ two }/>
       <img src={ three }/>
       <img src={ four }/>
