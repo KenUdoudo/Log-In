e02e922a46ccc3cfb31d039ed40fb9e8a49ba99f
Ken <kendo302003@gmail.com>
2023-07-08 08:06:26 +0100

    testing

--- src/Login.jsx
+++ src/Login.jsx
@@ -6,13 +6,16 @@ import { useState } from 'react'
 const LogIn = () => {
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
+  function login(){
+    console.warn(email, password)
+  }
   return (  
     <div className='flex flex-col justify-center items-center'>
       <img src={ evon } className='mt-10'/>
       <form className='flex flex-col justify-center items-center mt-20 gap-y-4'>
-        <input type="email" placeholder='Email' onChange={(e)=> setEmail(e.target.value)} className='border-0 rounded bg-gray-200 pl-4 py-1 pr-5 text-xl' required/>
-        <input type="password" placeholder='Password' className='border-0 rounded bg-gray-200 pl-4 py-1 pr-5 text-xl' onChange={(e)=> setPassword(e.target.value)} required/>
-        <button type="submit" className='border-0 rounded bg-blue-600 px-24 py-1 text-lg text-white'>Log In</button>
+        <input type="email" placeholder='email' onChange={(e)=> setEmail(e.target.value)} className='border-0 rounded bg-gray-200 pl-4 py-1 pr-5 text-xl' required/>
+        <input type="password" placeholder='password' className='border-0 rounded bg-gray-200 pl-4 py-1 pr-5 text-xl' onChange={(e)=> setPassword(e.target.value)} required/>
+        <button onClick={ login } className='border-0 rounded bg-blue-600 px-24 py-1 text-lg text-white'>Log In</button>
       </form>
       <p className='text-blue-600 mt-4'>Forgot Password?</p>
       <div className='flex flex-row justify-center items-center mt-4'>
