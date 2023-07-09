500eab5d34cac648560e1f7af49f70a440a4963b
Ken <kendo302003@gmail.com>
2023-07-09 09:11:01 +0100

    importing useState

--- src/Signup.jsx
+++ src/Signup.jsx
@@ -1,8 +1,11 @@
+import { useState } from 'react'
 import evon from './assets/logo (1).png'
 import facebook from './assets/logo (3).png'
 import google from './assets/logo (2).png'
 import { Link } from 'react-router-dom'
 const SignUp = () => {
+  const [name, setName] = useState("")
+
   return (  
     <div className='flex flex-col justify-center items-center'>
       <img src={ evon } className='mt-14'/>
