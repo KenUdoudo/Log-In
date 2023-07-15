18fdd44c332366a1bb8fa5c20f0bdb0a4dea19cd
Ken <kendo302003@gmail.com>
2023-07-15 20:03:54 +0100

    done with border

--- src/skipfornow.jsx
+++ src/skipfornow.jsx
@@ -6,11 +6,11 @@ import five from './assets/wallpaperflare.com_wallpaper (7).jpg'
 const Skip = () => {
   return ( 
     <div className='flex flex-col justify-center items-center gap-y-4 mt-4'>
-      <img src={ One } className='w-3/5 lg:border-1 border-blue-600'/>
+      <img src={ One } className='w-3/5 lg:border-1 rounded border-blue-600'/>
       <img src={ two } className='w-3/5 lg:border-1 border-blue-600'/>
       <img src={ three } className='w-3/5 lg:border-1 border-blue-600'/>
-      <img src={ four } className='w-3/5'/>
-      <img src={ five } className='w-3/5'/>
+      <img src={ four } className='w-3/5 lg:border-1 border-blue-600'/>
+      <img src={ five } className='w-3/5 lg:border-1 border-blue-600'/>
     </div>
   );
 }
