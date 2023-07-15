4dabd845d9ab9a8d5c6ec72da3af8bc318b4544b
Ken <kendo302003@gmail.com>
2023-07-15 20:02:03 +0100

    img three and correction

--- src/skipfornow.jsx
+++ src/skipfornow.jsx
@@ -7,8 +7,8 @@ const Skip = () => {
   return ( 
     <div className='flex flex-col justify-center items-center gap-y-4 mt-4'>
       <img src={ One } className='w-3/5 lg:border-1 border-blue-600'/>
-      <img src={ two } className='w-3/5 lg:border-1 border-blue-600''/>
-      <img src={ three } className='w-3/5'/>
+      <img src={ two } className='w-3/5 lg:border-1 border-blue-600'/>
+      <img src={ three } className='w-3/5 lg:border-1 border-blue-600'/>
       <img src={ four } className='w-3/5'/>
       <img src={ five } className='w-3/5'/>
     </div>
